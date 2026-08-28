13098af5e064c8426f9d5f746de62366a25799ff
ackarr <asbroas5@gmail.com>
2026-08-28 16:44:02 +0300

    correct random tests

--- SolveSolution2/SolveSolution.cpp
+++ SolveSolution2/SolveSolution.cpp
@@ -4,17 +4,16 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 #define QUANT_RIGHT_DOUBLE 3
-
-#define QUANT_TESTS 6
 
 #define MAX_RANDOM_NUM 10000
 
 enum NumberOfRoots
 {
-    BEGIN_ROOTS = -1,
-    INFINITY_ROOTS = -1,
+    INITIAL_ROOTS = -2,
+    INFINITY_ROOTS,
     ZERO_ROOTS,
     ONE_ROOTS,
     TWO_ROOTS
@@ -22,17 +21,17 @@ enum NumberOfRoots
 
 struct EquationParam
 {
-        double a, b, c;
-        int number_of_roots;
-        double x1, x2;
+    double a, b, c;
+    NumberOfRoots number_of_roots;
+    double x1, x2;
 };
 
 
-const double EPSILON = 1e-4;
+const double EPSILON = 1e-6;
 
 bool   IsCorEnter       (EquationParam* const parametrs);                        //Input
 
-void   EnterOneCoef     (double* const entered_coef, int* const quant_enter_double, const char used_letter);
+void   EnterOneCoef     (double* const entered_coef, const char used_letter);
 
 
 int    SolveEquation    (EquationParam* const parametrs);                       //Solving
@@ -44,7 +43,7 @@ int    Square           (EquationParam* const parametrs);
 double Discriminant     (const double a, const double b, const double c);
 
 
-bool   IsEqualDouble    (const double num1, const double num2);                     //Service function
+bool   IsEqualDouble    (const double num1, const double num2);                //Service function
 
 bool   IsItSpace        (const int check_num);
 
@@ -56,27 +55,28 @@ void   OutputRoots      (EquationParam parametrs);                     //Output
 
 void   RunAllTests      ();                                            //Tests
 
-void   RunTest          (EquationParam test);
+int    RunTest          (EquationParam test);
 
 int    RunTestRand      (EquationParam test);
+
+int    RunRandTest      (EquationParam parametrs);
+
+bool   IsZeroEquation   (double a, double b, double c, double x);
+
+
+double GenRandDouble    ();                                             //Random Generaation
 
 
 int main()
 {
-    /*double a = NAN, b = NAN, c = NAN;
-    double x1 = NAN, x2 = NAN;
-
-    int number_of_roots = 0;
-    */
-
-/*    EquationParam parametrs = {.a = NAN, .b = NAN, .c = NAN, .number_of_roots = BEGIN_ROOTS, .x1 = NAN, .x2 = NAN};
+    EquationParam parametrs = {.a = NAN, .b = NAN, .c = NAN, .number_of_roots = INITIAL_ROOTS, .x1 = NAN, .x2 = NAN};
 
     IsCorEnter(&parametrs);
 
     SolveEquation(&parametrs);
 
     OutputRoots(parametrs);
-*/
+
     RunAllTests();
 
     return 0;
@@ -87,38 +87,41 @@ int main()
 bool IsCorEnter(EquationParam* const parametrs)
 {
     printf("Enter coefficients\n");
-    int quant_enter_of_double = 0;
-    double* used_coef[] = {&(parametrs->a), &(parametrs->b), &(parametrs->c)};
+    double* used_coef [QUANT_RIGHT_DOUBLE] = {&(parametrs->a), &(parametrs->b), &(parametrs->c)};
 
     for(int i = 0; i < QUANT_RIGHT_DOUBLE; i++)
     {
         char used_letter = (char)('a' + i);
 
-        printf("Введите коэффициент %c \n", used_letter);
-
-        EnterOneCoef(used_coef[i], &quant_enter_of_double, used_letter);
+        EnterOneCoef(used_coef[i], used_letter);
     }
 
     return true;
 }
 
-void EnterOneCoef(double* const entered_coef, int* const quant_enter_of_double, const char used_letter)
+void EnterOneCoef(double* const entered_coef, const char used_letter)
 {
-    //bool is_clean_buf = DeleteBuf();
-    while(scanf("%lg", entered_coef) != 1 && !DeleteBuf())
+    printf("Input coefficient %c: ", used_letter);
+
+    int is_scan1 = scanf("%lg", entered_coef);
+    bool is_clean_buf = DeleteBuf();
+
+    while(is_scan1 != 1 || !is_clean_buf)
     {
-        printf("Enter good number for %c\n", used_letter);
-        //is_clean_buf = DeleteBuf();
+        printf("Enter good number for %c: ", used_letter);
+        is_scan1 = scanf("%lg", entered_coef);
+        is_clean_buf = DeleteBuf();
     }
-
-    ++(*quant_enter_of_double);
-
 }
 
 
 
 int SolveEquation(EquationParam* const parametrs)
 {
+    assert(!isnan(parametrs->a));
+    assert(!isnan(parametrs->b));
+    assert(!isnan(parametrs->c));
+
     if(IsEqualDouble(parametrs->a, 0.0) && IsEqualDouble(parametrs->b, 0.0) && IsEqualDouble(parametrs->c, 0.0))
     {
         parametrs->number_of_roots = INFINITY_ROOTS;
@@ -140,14 +143,14 @@ int SolveEquation(EquationParam* const parametrs)
         Square(parametrs);
     }
 
-    //assert(parametrs->number_of_roots < 0);
+    assert(parametrs->number_of_roots != INITIAL_ROOTS);
 
     return 0;
 }
 
 double Linear (const double slope, const double intercept)
 {
-    //assert(slope == 0);
+    assert(slope != 0);
     return - intercept / slope;
 }
 
@@ -157,26 +160,26 @@ int Square (EquationParam* const parametrs)
 
     discriminant = Discriminant(parametrs->a, parametrs->b, parametrs->c);
 
-
     if(discriminant < 0.0 && !IsEqualDouble(discriminant, 0.0))
     {
         parametrs->number_of_roots = ZERO_ROOTS;
     }
 
-    else if(IsEqualDouble(discriminant, 0.0) && !IsEqualDouble(parametrs->a, 0.0))
+    else if(IsEqualDouble(discriminant, 0.0))
     {
         parametrs->x1 = - parametrs->b / (2 * parametrs->a);
         parametrs->number_of_roots = ONE_ROOTS;
     }
 
-    else if(! IsEqualDouble(parametrs->a, 0.0))
+    else
     {
         double corenb = sqrt(discriminant);
+
         parametrs->x1 = (- parametrs->b + corenb) / (2 * parametrs->a);
         parametrs->x2 = (- parametrs->b - corenb) / (2 * parametrs->a);
-
         parametrs->number_of_roots = TWO_ROOTS;
     }
+
     return 0;
 }
 
@@ -205,18 +208,13 @@ bool DeleteBuf()
 
     while ((empty_letter = getchar()) != '\n')
     {
-        if(!IsItSpace(empty_letter))
+        if(!isspace(empty_letter))
         {
             flag = false;
         }
     }
 
     return flag;
-}
-
-bool IsItSpace(const int check_num)
-{
-    return (check_num == ' ' || check_num == '\t');
 }
 
 
@@ -227,7 +225,7 @@ void OutputRoots(EquationParam parametrs)
     {
         case ZERO_ROOTS:
         {
-            printf("Корней нет \n");
+            printf("Zero roots\n");
             break;
         }
 
@@ -245,7 +243,7 @@ void OutputRoots(EquationParam parametrs)
 
         case INFINITY_ROOTS:
         {
-            printf("Корней бесконечно много \n");
+            printf("Infinity roots\n");
             break;
         }
 
@@ -263,157 +261,66 @@ void OutputRoots(EquationParam parametrs)
 
 void RunAllTests()
 {
-    EquationParam tests_list [QUANT_TESTS] =
+    EquationParam tests_list [] =
     {
-    {.a = 0.0, .b = 0.0, .c = 0.0, .number_of_roots = INFINITY_ROOTS, .x1 = NAN, .x2 = NAN},
-    {.a = 1,   .b = 2,   .c = 1,   .number_of_roots = ONE_ROOTS,      .x1 = -1,  .x2 = NAN},
-    {.a = 0.0, .b = 2,   .c = -2,  .number_of_roots = ONE_ROOTS,      .x1 = 1,   .x2 = NAN},
-    {.a = 0.0, .b = 0.0, .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN},
-    {.a = 3,   .b = 2,   .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN},
-    {.a = 1,   .b = -5,  .c = 6,   .number_of_roots = TWO_ROOTS,      .x1 = 2,   .x2 = 3  },
+        {.a = 0.0, .b = 0.0, .c = 0.0, .number_of_roots = INFINITY_ROOTS, .x1 = NAN, .x2 = NAN},
+        {.a = 1,   .b = 2,   .c = 1,   .number_of_roots = ONE_ROOTS,      .x1 = -1,  .x2 = NAN},
+        {.a = 0.0, .b = 2,   .c = -2,  .number_of_roots = ONE_ROOTS,      .x1 = 1,   .x2 = NAN},
+        {.a = 0.0, .b = 0.0, .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN},
+        {.a = 3,   .b = 2,   .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN},
+        {.a = 1,   .b = -5,  .c = 6,   .number_of_roots = TWO_ROOTS,      .x1 = 2,   .x2 = 3  },
     };
 
     int tests_size = sizeof(tests_list) / sizeof(EquationParam);
 
+    int test_quant = 0;
+
     for(int i = 0; i < tests_size; i++)
     {
-        RunTest(tests_list[i]);
+        test_quant = test_quant + RunTest(tests_list[i]);
     }
 
-    int quant_cor_r = 0;
+    printf("Quantity correct hand test %d\n", test_quant);
+
+
+    int test_quant_r = 0;
 
     srand(time(0));
 
     for(int i = 0; i < 1000; i++)
     {
-        double a_r = (double) ((rand() % (2 * MAX_RANDOM_NUM) - (MAX_RANDOM_NUM))) / 100;
-        double b_r = (double) ((rand() % (2 * MAX_RANDOM_NUM) - (MAX_RANDOM_NUM))) / 100;
-        double c_r = (double) ((rand() % (2 * MAX_RANDOM_NUM) - (MAX_RANDOM_NUM))) / 100;
+        //printf("%lg %lg %lg\n", random_param.a, random_param.b, random_param.c);
+        EquationParam random_param = {.a = GenRandDouble(), .b = GenRandDouble(), .c = GenRandDouble(),
+                                      .number_of_roots = INITIAL_ROOTS, .x1 = NAN, .x2 = NAN};
 
-        //printf("%lg %lg %lg\n", a_r, b_r, c_r);
-        EquationParam random_param = {.a = a_r, .b = b_r, .c = c_r,
-                                      .number_of_roots = BEGIN_ROOTS, .x1 = NAN, .x2 = NAN};
-
-        SolveEquation(&random_param);
-
-
-        /*
-        if(isnan(random_param.x1))
-        {
-            if(isnan(random_param.x2))
-            {
-                if(random_param.number_of_roots == ZERO_ROOTS || random_param.number_of_roots == ZERO_ROOTS)
-                   quant_cor_r = quant_cor_r + 1;
-                else
-                    printf("k1");
-            }
-
-            else
-            {
-                printf("k2");
-            }
-        }
-
-        else
-        {
-            if(IsEqualDouble(a_r * random_param.x1 * random_param.x1 + b_r * random_param.x1 + c_r, 0.0))
-            {
-                if(isnan(random_param.x2) && random_param.number_of_roots == 1)
-                {
-                    quant_cor_r = quant_cor_r + 1;
-                }
-
-                if(IsEqualDouble(a_r * random_param.x2 * random_param.x2 + b_r * random_param.x2 + c_r, 0.0)
-                   && random_param.number_of_roots == 2)
-                    quant_cor_r = quant_cor_r + 1;
-            }
-        }
-        */
-
-
-        switch(random_param.number_of_roots)
-        {
-        case ZERO_ROOTS:
-        {
-            if(isnan(random_param.x1) && isnan(random_param.x2))
-                quant_cor_r = quant_cor_r + 1;
-            break;
-        }
-
-        case ONE_ROOTS:
-        {
-            if(isnan(random_param.x2)
-            && IsEqualDouble(a_r * random_param.x1 * random_param.x1 + b_r * random_param.x1 + c_r, 0.0))
-                quant_cor_r = quant_cor_r + 1;
-            break;
-        }
-
-        case TWO_ROOTS:
-        {
-            if(IsEqualDouble(a_r * random_param.x1 * random_param.x1 + b_r * random_param.x1 + c_r, 0.0)
-            && IsEqualDouble(a_r * random_param.x2 * random_param.x2 + b_r * random_param.x2 + c_r, 0.0))
-                quant_cor_r = quant_cor_r + 1;
-            break;
-        }
-
-        case INFINITY_ROOTS:
-        {
-            if(isnan(random_param.x1) && isnan(random_param.x2)
-               && IsEqualDouble(a_r, 0.0) && IsEqualDouble(b_r, 0.0) && IsEqualDouble(c_r, 0.0))
-                quant_cor_r = quant_cor_r + 1;
-            break;
-        }
-
-        default:
-        {
-            printf("wtf");
-            break;
-        }
-        }
+        test_quant_r = test_quant_r + RunRandTest(random_param);
     }
-
-    printf("Quantity correct random test %d\n", quant_cor_r);
-
-
-
-
-    /*EquationParam test1 = {.a = 0.0, .b = 0.0, .c = 0.0, .number_of_roots = INFINITY_ROOTS, .x1 = NAN, .x2 = NAN};
-    EquationParam test2 = {.a = 1,   .b = 2,   .c = 1,   .number_of_roots = ONE_ROOTS,      .x1 = -1,  .x2 = NAN};
-    EquationParam test3 = {.a = 0.0, .b = 2,   .c = -2,  .number_of_roots = ONE_ROOTS,      .x1 = 1,   .x2 = NAN};
-    EquationParam test4 = {.a = 0.0, .b = 0.0, .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN};
-    EquationParam test5 = {.a = 3,   .b = 2,   .c = 1,   .number_of_roots = ZERO_ROOTS,     .x1 = NAN, .x2 = NAN};
-    EquationParam test6 = {.a = 1,   .b = -5,  .c = 6,   .number_of_roots = TWO_ROOTS,      .x1 = 2,   .x2 = 3  };
-
-
-    RunTest(test1);
-    RunTest(test2);
-    RunTest(test3);
-    RunTest(test4);
-    RunTest(test5);
-    RunTest(test6);
-    */
+    printf("Quantity correct random test %d\n", test_quant_r);
 }
 
-void RunTest(EquationParam test)
+
+int RunTest(EquationParam test)
 {
 
         EquationParam parametrs_testing = {.a = test.a, .b = test.b, .c = test.c,
-                                           .number_of_roots = 0, .x1 = NAN, .x2 = NAN};
+                                           .number_of_roots = INITIAL_ROOTS, .x1 = NAN, .x2 = NAN};
 
         SolveEquation(&parametrs_testing);
 
         bool x1_cor = false, x2_cor = false, quant_roots_cor = false;
 
         x1_cor = IsEqualDouble(parametrs_testing.x1, test.x1)
-                 || (IsEqualDouble(parametrs_testing.x2, test.x1) && IsEqualDouble(parametrs_testing.x1, test.x2));
+             || (IsEqualDouble(parametrs_testing.x2, test.x1) && IsEqualDouble(parametrs_testing.x1, test.x2));
 
         x2_cor = IsEqualDouble(parametrs_testing.x2, test.x2)
-                 || (IsEqualDouble(parametrs_testing.x2, test.x1) && IsEqualDouble(parametrs_testing.x1, test.x2));
+             || (IsEqualDouble(parametrs_testing.x2, test.x1) && IsEqualDouble(parametrs_testing.x1, test.x2));
 
         quant_roots_cor = parametrs_testing.number_of_roots == test.number_of_roots;
 
         if(x1_cor && x2_cor && quant_roots_cor)
-            printf("Test was end without errors \n");
+        {
+            return 1;
+        }
 
         else
         {
@@ -425,15 +332,71 @@ void RunTest(EquationParam test)
             test.a, test.b, test.c, test.x1,
             test.x2, test.number_of_roots,
             parametrs_testing.x1, parametrs_testing.x2, parametrs_testing.number_of_roots);
+
+            return 0;
         }
 
 }
 
+int RunRandTest(EquationParam random_param)
+{
+    SolveEquation(&random_param);
+
+    switch(random_param.number_of_roots)
+    {
+        case ZERO_ROOTS:
+        {
+            if(isnan(random_param.x1) && isnan(random_param.x2))
+                return 1;
+            break;
+        }
+
+        case ONE_ROOTS:
+        {
+            if(isnan(random_param.x2)
+            && IsZeroEquation(random_param.a, random_param.b, random_param.c, random_param.x1))
+                return 1;
+            break;
+        }
+
+        case TWO_ROOTS:
+        {
+            if(IsZeroEquation(random_param.a, random_param.b, random_param.c, random_param.x1)
+            && IsZeroEquation(random_param.a, random_param.b, random_param.c, random_param.x2))
+                return 1;
+            break;
+        }
+
+        case INFINITY_ROOTS:
+        {
+            if(isnan(random_param.x1) && isnan(random_param.x2)
+                && IsEqualDouble(random_param.a, 0.0)
+                && IsEqualDouble(random_param.b, 0.0)
+                && IsEqualDouble(random_param.c, 0.0))
+                return 1;
+            break;
+        }
+
+        default:
+        {
+            printf("wtf");
+            return 0;
+        }
+    }
+    return 0;
+}
+
+bool IsZeroEquation(double a, double b, double c, double x)
+{
+    return IsEqualDouble(a * x * x + b * x + c, 0.0);
+}
 
 
 
-
-
+double GenRandDouble()
+{
+    return (double) ((rand() % (2 * MAX_RANDOM_NUM) - (MAX_RANDOM_NUM))) / 100;
+}
 
 
 
